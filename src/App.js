df76284fe33c11f45c00995d7d38aac02eebbc69
Eryn Gautier <gauti22e@mtholyoke.edu>
2021-06-03 15:49:17 -0400

    Refactor names

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 function App() {
   const [counter, updateCounter] = useState(0);
+  var namesArr = ["Eryn", "Cindy", "Shagun"];
 
   return (
     <div style={{padding: "2.5em"}}>
@@ -13,107 +14,15 @@ function App() {
       <div style={{paddingLeft: "2.5em", borderTop: "1px solid royalblue"}}>
 
         <div style={{paddingTop: "1.25em"}}>
-            <UserCard
-            name={'Eryn'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-      
-            <UserCard
-            name={'Maya'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-     
-            <UserCard
-            name={'Cirill'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Michelle'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Shagun'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Cindy'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Shaniah'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Sophie'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Gabriela'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Kriti'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Rishika'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Isabel'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Corinne'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Shannon'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Claire'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Priska'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
-
-            <UserCard
-            name={'Trang'}
-            counter={counter}
-            updateCounter={updateCounter}
-            />
+            {namesArr.map((name) => {
+              return(
+                <UserCard
+                  name={name}
+                  counter={counter}
+                  updateCounter={updateCounter}
+                  />
+              );
+            })}
         </div>
       </div>
       <button onClick={()=> {console.log(counter)}}>{retStr("-- > Log Count < --")}</button>
